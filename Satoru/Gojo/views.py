946916bd9fcd6a8e8b65e024ab815b285ab16ec6
MitraLothbrok <109756044+MitraLothbrok@users.noreply.github.com>
2024-04-14 13:41:12 +0300

    init

---
+++ Satoru/Gojo/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def mainpage(response):
+    return HttpResponse('...')
+
+
+def analyse(response):
+    return HttpResponse('...')
+
+def story(response):
+    return HttpResponse('...')
+
+def arts(response):
+    return HttpResponse('...')
+
---
+++ Satoru/Satoru/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from Gojo.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Gojo.urls'))
+]
